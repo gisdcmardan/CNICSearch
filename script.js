6af04004980c3e33c4509f3aafced9cb1e92ebb7
gisdcmardan <gisdcmardan@gmail.com>
2026-03-13 09:36:00 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+async function searchCNIC() {
+
+let input = document.getElementById("search").value;
+
+let response = await fetch("database.json");
+let data = await response.json();
+
+let result = data.find(r => r.CNIC == input);
+
+if(result){
+
+document.getElementById("result").innerHTML =
+`
+Name: ${result.Name} <br>
+CNIC: ${result.CNIC} <br>
+Mobile: ${result.Mobile} <br>
+Transaction: ${result["Payment Transaction ID"]}
+`;
+
+}
+
+else{
+
+document.getElementById("result").innerHTML =
+"Record not found";
+
+}
+
+}
